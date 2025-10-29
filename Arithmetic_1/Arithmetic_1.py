9a3d7ad874b2f71fe8702b6182f2bc76ba6a8445
JananiRavi <janujananiravi@gmail.com>
2025-10-29 17:55:16 +0530

    Create Arithmetic_1.py

---
+++ Arithmetic_1/Arithmetic_1.py
@@ -0,0 +1,9 @@
+a= float(input("enter first number:"))
+b= float(input("enter second number:"))
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+if b!=0:
+    print("division:",a/b)
+else:
+    print("not divisible by zero")    
