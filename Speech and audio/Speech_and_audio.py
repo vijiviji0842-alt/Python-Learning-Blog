78ee458295a715f5fa9b20d808bec4607561e32b
Jayavarthini karthikeyan <vijiviji0842@gmail.com>
2025-10-30 08:41:12 +0530

    Add comments for duration and threshold variables
    Added comments to clarify duration and threshold variables.

--- Speech and audio/Speech_and_audio.py
+++ Speech and audio/Speech_and_audio.py
@@ -1,8 +1,8 @@
 import sounddevice as sd
 import numpy as np
 
-duration = 0.5  
-threshold = 0.3  
+duration = 0.5  # listening duration ( second)
+threshold = 0.3  # loudness limit to detect the clap
 
 print("Listening for Clap...")
 
